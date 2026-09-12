628af9a5bb463732c328ca5f080fcfd50d21ff68
Samiksha <samikshakaushik5@gmail.com>
2026-09-13 02:38:48 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ def get_db_connection():
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
-database=os.getenv("DB_NAME"),
+        database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-password=os.getenv("DB_PASSWORD"),
+        password=os.getenv("DB_PASSWORD"),
+        sslmode=os.getenv("DB_SSLMODE", "prefer"),
     )
 @app.route("/")
 def home():
